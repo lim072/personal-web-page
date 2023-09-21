2924027f67096ce3e43bb2f57635b24dd43f5c66
lim072 <bestbro072@gmail.com>
2023-09-21 18:38:14 +0900

    개인페이지 메뉴 js 작업

--- script/nav.js
+++ script/nav.js
@@ -1,13 +1,34 @@
 const menuBtn = document.querySelector(".menuBtn");
 const menu = document.querySelector(".menu");
-const close = menu.querySelector("span");
+const close = menu.querySelector(".close");
 const visual = document.querySelector(".mainVisual");
+const meues = menu.querySelectorAll("ul>li");
+const visuals = visual.querySelectorAll("section");
+
+meues.forEach((el,index) =>{
+    el.addEventListener("click",()=>{
+        
+        for(let el of visuals){
+            el.classList.remove("on");
+            visuals[index].classList.add("on");
+        }
+        menu.style.transform = "rotateY(-90deg)";
+        visual.style.transform = "rotateY(0deg)";
+        menuBtn.style.opacity = "1"
+        visual.style.backgroundColor = "rgba(255, 255, 255, 0)";
+    })
+})
 
 menuBtn.addEventListener("click", ()=>{
         menu.style.transform = "rotateY(-350deg)";
         menuBtn.style.opacity = "0"
+        visual.style.transform = "rotateY(-10deg)";
+        visual.style.backgroundColor = "rgba(255, 255, 255, 0.07)";
 })
 close.addEventListener("click", ()=>{
     menu.style.transform = "rotateY(-90deg)";
     menuBtn.style.opacity = "1"
+    visual.style.transform = "rotateY(0deg)";
+    visual.style.backgroundColor = "rgba(255, 255, 255, 0)";
 })
+
